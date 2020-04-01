f26def8301fe8f8f6c685dce52f2f86847a1b6b7
Edyta Dąbrowska <editt.dabrowska@gmail.com>
2020-04-01 20:02:17 +0200

    add js and docs file

---
+++ src/app.js
@@ -0,0 +1,12 @@
+document.addEventListener('DOMContentLoaded', function () {
+  const closeBtn = document.getElementById('close-btn');
+  const subscribeBtn = document.getElementById('subscribe-btn');
+
+  closeBtn.addEventListener('click', () => {
+    console.log('close');
+  });
+
+  subscribeBtn.addEventListener('click', () => {
+    console.log('subscribe');
+  })
+});
